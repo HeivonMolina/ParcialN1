cfa7a384324d86685cc82da0eab2a1d03b89c4b7
HeivonMolina <65148622+HeivonMolina@users.noreply.github.com>
2020-10-05 14:59:22 -0500

    NO se puede registrar producto repetido - Prueba

--- ComidasRapitas.Tests/RegistroComprasTests.cs
+++ ComidasRapitas.Tests/RegistroComprasTests.cs
@@ -19,5 +19,15 @@ namespace ComidasRapitas.Tests
             var resultado = comprar.Registrar(ProductoSimple);
             Assert.Pass("Registrado Correctamente",resultado);
         }
+        public void NoPuedoRegistrarProductoRepetido()
+        {
+            var ProductoSimple = new ProductoSimple(100, "Salchicha", 1000, 2000);
+            var comprar = new Compra();
+            comprar.Registrar(ProductoSimple);
+            var ProductoSimple2 = new ProductoSimple(10, "Salchicha", 1000, 2000);
+            var resultado = comprar.Registrar(ProductoSimple2);
+
+            Assert.Pass("El Producto Salchicha ya se ha registrado anteriormente", resultado);
+        }
     }
 }
